c41b2feed4c5d700fbd5048b8188ffafc86fbb32
Karolina Laskowska <karolina.laskowska19@gmail.com>
2022-11-21 15:01:36 +0100

    Add start screen

--- sokoban.hs
+++ sokoban.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 import CodeWorld
 
+startScreen :: Picture
+startScreen = scaled 3 3 (lettering "Sokoban!")
+
 cloud :: Picture
 cloud = pictures [translated x y (colored white (solidCircle 0.15))
   | x <- [-0.25, 0, 0.25], y <- [0]]
@@ -45,8 +48,6 @@ drawTile Box = box False
 drawTile Blank = blankField
 
 data Coord = C {x, y :: Integer}
-initialState :: (Direction, Coord)
-initialState = (U, C 0 1)
 
 maze :: Coord -> Tile
 maze c
@@ -60,37 +61,16 @@ maze c
 pictureOfMaze :: Picture
 pictureOfMaze = pictures [translated (fromIntegral x) (fromIntegral y) (drawTile (maze (C x y))) 
   | x <- [-10 .. 10], y <- [-10 .. 10]]
-  
-player1 :: Picture
-player1 = polyline [(0, -0.4), (0, 0.4)] & polyline [(-0.2, 0.2), (0, 0.4), (0.2, 0.2)]
 
 data Direction = R | U | L | D
 
-adjacentCoords :: Direction -> Coord -> (Coord, Coord)
-adjacentCoords R (C x y) = (C x y, C (x+1) y)
-adjacentCoords U (C x y) = (C x y, C x (y+1))
-adjacentCoords L (C x y) = (C x y, C (x-1) y)
-adjacentCoords D (C x y) = (C x y, C x (y-1))
+initialCoord :: Coord
+initialCoord = C 0 1
 
-newPosition :: Direction -> Coord -> Coord
-newPosition d c
-  | isMovePossible (maze (snd movedCoord)) = snd movedCoord
-  | otherwise = fst movedCoord
-  where movedCoord = adjacentCoords d c
+data Position = P {dir :: Direction, coord :: Coord}
 
-handleEvent :: Event -> (Direction, Coord) -> (Direction, Coord)
-handleEvent (KeyPress key) (d, c)
-    | key == "Right" = (R, newPosition R c)
-    | key == "Up"    = (U, newPosition U c)
-    | key == "Left"  = (L, newPosition L c)
-    | key == "Down"  = (D, newPosition D c)
-handleEvent _ x      = x
-
-isMovePossible tile =
-  case tile of
-    Ground -> True
-    Storage -> True
-    _ -> False
+initialState2 :: Position
+initialState2 = P U (C 0 1)
 
 rotate :: Direction -> Picture -> Picture
 rotate d p =
@@ -100,18 +80,121 @@ rotate d p =
     L -> rotated (pi * 0.5) p
     D -> rotated pi p
 
+player1 :: Picture
+player1 = polyline [(0, -0.4), (0, 0.4)] & polyline [(-0.2, 0.2), (0, 0.4), (0.2, 0.2)]
+
+player2 :: Direction -> Picture
+player2 d = rotate d player1
+
+adjacentCoords :: Direction -> Coord -> (Coord, Coord)
+adjacentCoords R (C x y) = (C x y, C (x+1) y)
+adjacentCoords U (C x y) = (C x y, C x (y+1))
+adjacentCoords L (C x y) = (C x y, C (x-1) y)
+adjacentCoords D (C x y) = (C x y, C x (y-1))
+
+isMovePossible tile =
+  case tile of
+    Ground -> True
+    Storage -> True
+    _ -> False
+
+newPosition :: Direction -> Coord -> Coord
+newPosition d c
+  | isMovePossible (maze (snd movedCoord)) = snd movedCoord
+  | otherwise = fst movedCoord
+  where movedCoord = adjacentCoords d c
+
+handleEvent1 :: Event -> Coord -> Coord
+handleEvent1 (KeyPress key) c
+    | key == "Right" = newPosition R c
+    | key == "Up"    = newPosition U c
+    | key == "Left"  = newPosition L c
+    | key == "Down"  = newPosition D c
+handleEvent1 _ c      = c
+
+handleEvent2 :: Event -> Position -> Position
+handleEvent2 (KeyPress key) pos
+    | key == "Right" = go R
+    | key == "Up"    = go U
+    | key == "Left"  = go L
+    | key == "Down"  = go D
+    where go dir = P dir (newPosition dir (coord pos))
+handleEvent2 _ x      = x
+
 atCoord :: Coord -> Picture -> Picture
 atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
 
-drawState (d, c) = atCoord c (rotate d player1) & pictureOfMaze
+drawState1 :: Coord -> Picture
+drawState1 c = atCoord c player1 & pictureOfMaze
+
+drawState2 :: Position -> Picture
+drawState2 pos = atCoord (coord pos) (player2 (dir pos)) & pictureOfMaze
 
 walk1 :: IO ()
-walk1 = activityOf initialState handleEvent drawState
+walk1 = activityOf initialCoord handleEvent1 drawState1
+
+isEscapePressed (KeyPress key)
+  | key == "Esc" = True
+isEscapePressed _ = False
+
+isEscapeReleased (KeyRelease key)
+  | key == "Esc" = True
+isEscapeReleased _ = False
+
+-- the game is reseted when `Esc` is pressed or released
+resettableHandleEvent :: 
+  world -> 
+  (Event -> world -> world) -> 
+  (Event -> world -> world)
+resettableHandleEvent initialPos handleEvent = (\evn pos -> 
+  if ((isEscapePressed evn) || (isEscapeReleased evn)) 
+    then handleEvent evn initialPos 
+  else handleEvent evn pos)
+
+data SSState world = StartScreen | Running world
+
+data Activity world = Activity {
+  actState  :: world,
+  actHandle :: (Event -> world -> world),
+  actDraw   ::(world -> Picture)
+}
+
+resettable :: Activity s -> Activity s
+resettable (Activity state0 handle draw)
+  = Activity state0 handle' draw
+  where handle' (KeyPress key) _ | key == "Esc" = state0
+        handle' e s = handle e s
+
+withStartScreen (Activity state0 handle draw)
+  = Activity state0' handle' draw'
+  where
+    state0' = StartScreen
+
+    handle' (KeyPress key) StartScreen
+         | key == " "                  = Running state0
+    handle' _              StartScreen = StartScreen
+    handle' e              (Running s) = Running (handle e s)
+
+    draw' StartScreen = startScreen
+    draw' (Running s) = draw s
+
+runActivity :: Activity s -> IO ()
+runActivity (Activity state0 handle draw)
+  = activityOf state0 handle draw
+
+walk2 :: IO ()
+walk2 = runActivity (Activity initialState2 handleEvent2 drawState2)
+
+walk3 :: IO ()
+walk3 = runActivity (resettable (Activity initialState2 handleEvent2 drawState2))
+
+walk4 :: IO ()
+walk4 = runActivity (resettable (withStartScreen (Activity initialState2 handleEvent2 drawState2)))
 
 type Program = IO ()
 
 program :: Program
-program = walk1
+program = walk3
 
 main :: Program
 main = program
